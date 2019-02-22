3f014cb54a99257e7f2a6d04679e5f3aa3857aa0
jmpainter <jpainter4@gmail.com>
2019-02-22 15:57:43 -0800

    added flight times

---
+++ hash-maps/flight-times.js
@@ -0,0 +1,18 @@
+function chooseMovies(movieTimes, flightTime) {
+  const diffMap = {};
+  debugger;
+  for (let time of movieTimes) {
+    const diff = flightTime - time;
+    if (diffMap[diff]) {
+      return [time, diff];
+    } else {
+      diffMap[time] = 1;
+    }
+  }
+  return null;
+}
+
+// const movieTimes = [40, 30, 60, 20];
+const movieTimes = [30];
+
+console.log(chooseMovies(movieTimes, 60));
