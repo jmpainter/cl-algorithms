1521afe87b2b8a19bf4f1c98810238129c666111
jmpainter <jpainter4@gmail.com>
2019-03-05 17:36:39 -0800

    modified product-of-all-other

--- greedy-algorithms/product-of-all-other.js
+++ greedy-algorithms/product-of-all-other.js
@@ -1,31 +1,21 @@
 function getProductsOfAllIntsExceptAtIndex(intArray) {
   if (intArray.length < 2) {
-    throw new Error(
-      'Getting the product of numbers at other indices requires at least 2 numbers'
-    );
+    throw new Error('Array must have length of at least 2');
   }
-  debugger;
-  const productsOfAllIntsExceptAtIndex = [];
+  const result = [];
 
-  // For each integer, we find the product of all the integers
-  // before it, storing the total product so far each time
   let productSoFar = 1;
   for (let i = 0; i < intArray.length; i++) {
-    productsOfAllIntsExceptAtIndex[i] = productSoFar;
+    result[i] = productSoFar;
     productSoFar *= intArray[i];
   }
 
-  // For each integer, we find the product of all the integers
-  // after it. since each index in products already has the
-  // product of all the integers before it, now we're storing
-  // the total product of all other integers
   productSoFar = 1;
-  for (let j = intArray.length - 1; j >= 0; j--) {
-    productsOfAllIntsExceptAtIndex[j] *= productSoFar;
-    productSoFar *= intArray[j];
+  for (let i = intArray.length - 1; i >= 0; i--) {
+    result[i] *= productSoFar;
+    productSoFar *= intArray[i];
   }
-
-  return productsOfAllIntsExceptAtIndex;
+  return result;
 }
 
 const arr = [2, 6, 5, 9];
